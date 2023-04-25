e018d27c5cfa796ceceba425e74f3f04f3569907
andysama97 <131812624+andysama97@users.noreply.github.com>
2023-04-26 02:29:45 +0700

    Add files via upload
    just started learning. nothing to see here.

---
+++ userInput2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name, occupation, hobby;
+    int age;
+
+    cout << "your name : ";
+    getline(cin, name);
+    cout << "age : ";
+    cin >> age;
+    cout << "occupation : ";
+    getline(cin >> ws, occupation);
+    cout << "hobby : ";
+    getline(cin, hobby);
+
+    cout << "so you're name is " << name << ", you're " << age << " years old. \n";
+    cout << "currently, you work as " << occupation <<". And you love " << hobby << ". \n";
+    cout << "i like your data so far, i think you'll do just fine here. Welcome aboard!";
+
+    return 0;
+}
