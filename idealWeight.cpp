aa9fec05acb343bbf1fa7222f22c7639e57ddf9b
Andhika Putra <131812624+andysama97@users.noreply.github.com>
2023-08-17 21:08:29 +0700

    Update idealWeight.cpp

--- idealWeight.cpp
+++ idealWeight.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int identify() {
     int x;
-    cout << "Specify your gender (1 for female, 2 for male) : ";
+    cout << "Specify your gender (1 for female, 2 for male, 0 Abort application) : ";
     cin >> x;
     return x;
 }
@@ -13,7 +13,7 @@ void fem() {
     cout << "Enter your height (cm) : ";
     cin >> height;
     float idealWeight = (height - 100) - ((height - 100)*15/100);
-    cout << "Your ideal weight is " << idealWeight << " Kg.";
+    cout << "Your ideal weight is " << idealWeight << " Kg." << endl;
 }
 
 void male() {
@@ -21,7 +21,7 @@ void male() {
     cout << "Enter your height (cm) : ";
     cin >> height;
     float idealWeight = (height - 100) - ((height - 100)*10/100);
-    cout << "Your ideal weight is " << idealWeight << " Kg.";
+    cout << "Your ideal weight is " << idealWeight << " Kg." << endl;
 }
 
 int main() {
@@ -29,11 +29,18 @@ int main() {
 
     if (gender == 1) {
         fem();
+        system("PAUSE");
+        main();
      } else if (gender == 2) {
         male();
+        system("PAUSE");
+        main();
+     } else if (gender == 0) {
+        cout << "Aborting application . . " << endl;
+        system("PAUSE");
+        return 0;
      } else {
-        cout << "Command unrecognizable. Aborting..";
+        cout << "Command unrecognizable" << endl;
+        main();
      }
-    
-    return 0;
 }
